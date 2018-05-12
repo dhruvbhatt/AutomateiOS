820a6bc0b20cf72e107a7a924d096a5de8da2512
Dhruv Bhatt <dhruv.bhatt@vodafone.com>
2018-05-12 15:14:00 +0530

    Added test case.

--- TestAutomation/TestAutomationTests/TestAutomationTests.swift
+++ TestAutomation/TestAutomationTests/TestAutomationTests.swift
@@ -16,6 +16,12 @@ class TestAutomationTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    func testNumbers() {
+        let vc = ViewController()
+        let value = vc.addNumbers()
+        XCTAssertEqual(value, 9)
+    }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
